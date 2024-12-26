6fa5bf0248604cb748e4e8af4a3d93218e584ee3
Saikumar060211 <167053815+Saikumar060211@users.noreply.github.com>
2024-12-26 09:32:14 +0530

    Add files via upload

---
+++ Main.do while loop.java.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		int i=1;
+		do{
+		    System.out.println(i);
+		    i++;
+		}
+		while(i<=5);
+	}
+}
